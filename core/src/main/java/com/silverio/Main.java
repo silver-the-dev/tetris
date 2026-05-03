1a8dbfc51dcbe501b8958e5b0c127af295d5aa8f
SilverDeveloper <52933126+silver-the-dev@users.noreply.github.com>
2026-05-03 00:35:15 -0300

    initial commit

---
+++ core/src/main/java/com/silverio/Main.java
@@ -0,0 +1,41 @@
+package com.silverio;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+import static com.badlogic.gdx.graphics.Color.BLUE;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    private ShapeRenderer shapeRend;
+    private final int VELOCITY = 10;
+    private int posX;
+    private int posY;
+
+    @Override
+    public void create() {
+        shapeRend = new ShapeRenderer();
+        posX = 0;
+        posY = 0;
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        posX += ((Gdx.input.isKeyPressed(Input.Keys.D) ? 1 : 0) - (Gdx.input.isKeyPressed(Input.Keys.A) ? 1 : 0)) * VELOCITY;
+
+
+        shapeRend.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRend.setColor(BLUE);
+        shapeRend.rect(posX, posY, 100, 100);
+        shapeRend.end();
+    }
+
+    @Override
+    public void dispose() {
+        shapeRend.dispose();
+    }
+}
